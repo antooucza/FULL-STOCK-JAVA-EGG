e66402e2e492ed95bf2aab1d01603c11c7c05dcb
Antonio <ouczarcyn.anto@gmail.com>
2023-03-16 14:13:11 -0300

    Carga Inicial

---
+++ GUIA 3/Ejercicio Practicos/Ejercicio1/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número entero: ");
+        int numero = scanner.nextInt();
+
+        // Verificar si el número es par o impar
+        if (numero % 2 == 0) {
+            System.out.println(numero + " es un número par.");
+        } else {
+            System.out.println(numero + " es un número impar.");
+        }
+    }
+}
