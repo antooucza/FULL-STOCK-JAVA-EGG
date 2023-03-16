5725216ba9796d77868c995a291a35c0a6cf3979
Antonio <ouczarcyn.anto@gmail.com>
2023-03-16 14:27:06 -0300

    Carga Inicial

---
+++ GUIA 3/Ejercicios Extras/Ejercicio7parte2/src/Main.java
@@ -0,0 +1,33 @@
+// usando el bucle do-while
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de números a evaluar: ");
+        int n = scanner.nextInt();
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+
+        int i = 0;
+        do {
+            System.out.print("Ingrese un número: ");
+            int num = scanner.nextInt();
+
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            sum += num;
+
+            i++;
+        } while (i < n);
+
+        double avg = (double) sum / n;
+
+        System.out.println("El valor máximo es: " + max);
+        System.out.println("El valor mínimo es: " + min);
+        System.out.println("El promedio es: " + avg);
+    }
+}
